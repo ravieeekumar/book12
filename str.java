230b9807f3b17509fefc63635ef5383f0868c9f3
ravieeekumar <ravikumarseee@gmail.com>
2017-09-04 15:19:19 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fact
+{
+public static void main(String s[])
+{
+Scanner in=new Scanner(System.in);
+String s1=in.next();
+int n=Integer.parseInt(s1);
+System.out.print(n);
+}
+}
