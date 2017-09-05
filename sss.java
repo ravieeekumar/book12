3791af4a68f1f30d083b7c93ccd013e34a2acdbe
ravieeekumar <ravikumarseee@gmail.com>
2017-09-05 11:56:59 +0530

    Create sss.java

---
+++ sss.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sss
+{
+public static void main(String s[])
+{
+Scanner in=new Scanner(System.in);
+String s1=in.next();
+String s2=new StringBuffer(s1).reverse().toString();
+System.out.print(s2);
+}
+}
